597ad0b3d568e537bade6cf8de432d27f2d3f556
alterego <125763784+senyaml@users.noreply.github.com>
2024-10-24 09:45:36 -0700

    Update mount.sh

--- recovery/root/system/bin/mount.sh
+++ recovery/root/system/bin/mount.sh
@@ -5,13 +5,4 @@ sleep 5
 # Unmount
 umount -l /vendor
 
-# Mount system and vendor once to fix permissions
-mount /dev/block/mapper/vendor /vendor
-
-# Remount as read-write
-mount -o remount,rw /vendor
-
-# Unmount
-umount -l /vendor
-
 exit 0
